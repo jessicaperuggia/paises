184f3a84b2015668a01b5d064bbec810faf0eb0f
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-07-21 21:48:58 -0300

    create JS

---
+++ js/script.js
@@ -0,0 +1,74 @@
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCoutries = [];
+let favoriteCountries = [];
+
+let countCountries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFavorites = 0;
+
+let numberFormat = null;
+
+window.addEventListener('load', () => {
+    tabCountries = document.querySelector('#tabCountries');
+    tabFavorites = document.querySelector('#tabFavorites');
+    countCountries = document.querySelector('#countCountries');
+    countFavorites = document.querySelector('#countFavorites');
+
+    totalPopulationList = document.querySelector('#totalPopulationList');
+    totalPopulationFavorites = document.querySelector('#totalPopulationFavorites');
+
+    numberFormat = Intl.NumberFormat('pt-BR');
+
+    fetchCountries();
+});
+
+async function fetchCountries() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+    allCoutries = json.map(country => {
+        const { numericCode, translations, population, flag } = country;
+
+        return {
+            id: numericCode,
+            name: translations.pt,
+            population,
+            flag
+        };
+    });
+
+    render();
+}
+
+function render() {
+    renderCountryList();
+    renderFavorites();
+    renderSumary();
+
+    handleCountryButtons();
+}
+
+function renderCountryList() {
+    let countriesHTML = '<div>';
+
+    allCoutries.forEach(country => {
+        const { name, flag, id, population } = country;
+
+        const countryHTML = `
+            <div class='country'> 
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                </div>
+            </div>
+        `;
+    });
+}
+function renderFavorites() { }
+function renderSumary() { }
+function handleCountryButtons() { }
