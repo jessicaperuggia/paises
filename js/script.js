b99fc9b55a5f515cd7d7e5112118be559f04b920
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-07-22 12:02:29 -0300

    add and remove countries

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let tabCountries = null;
 let tabFavorites = null;
 
-let allCoutries = [];
+let allCountries = [];
 let favoriteCountries = [];
 
 let countCountries = 0;
@@ -29,13 +29,15 @@ window.addEventListener('load', () => {
 async function fetchCountries() {
     const res = await fetch('https://restcountries.eu/rest/v2/all');
     const json = await res.json();
-    allCoutries = json.map(country => {
+
+    allCountries = json.map(country => {
         const { numericCode, translations, population, flag } = country;
 
         return {
             id: numericCode,
             name: translations.pt,
             population,
+            formattedPopulation: formatNumber(population),
             flag
         };
     });
@@ -46,7 +48,7 @@ async function fetchCountries() {
 function render() {
     renderCountryList();
     renderFavorites();
-    renderSumary();
+    renderSummary();
 
     handleCountryButtons();
 }
@@ -54,21 +56,120 @@ function render() {
 function renderCountryList() {
     let countriesHTML = '<div>';
 
-    allCoutries.forEach(country => {
-        const { name, flag, id, population } = country;
+    allCountries.forEach(country => {
+        const { name, flag, id, population, formattedPopulation } = country;
 
         const countryHTML = `
             <div class='country'> 
                 <div>
+                    <a id="${id}" class="waves-effect waves-light btn">+</a>
                 </div>
                 <div>
+                    <img src="${flag}" alt="${name}">
                 </div>
                 <div>
+                    <ul>
+                        <li>${name}</li>
+                        <li>${formattedPopulation}</li>
+                    </ul>
                 </div>
             </div>
         `;
+
+        countriesHTML += countryHTML;
+    });
+
+    countriesHTML += '</div>';
+    tabCountries.innerHTML = countriesHTML;
+}
+
+function renderFavorites() {
+    let favoritesHTML = '<div>';
+
+    favoriteCountries.forEach(country => {
+        const { name, flag, id, population, formattedPopulation } = country;
+
+        const favoriteCountryHTML = `
+            <div class='country'>
+                <div>
+                    <a id="${id}" class="waves-effect waves-light btn red darken-4">-</a>
+                </div>
+                <div>
+                    <img src="${flag}" alt="${name}">
+                </div>
+                <div>
+                    <ul>
+                        <li>${name}</li>
+                        <li>${formattedPopulation}</li>
+                    </ul>
+                </div>
+            </div>
+        `;
+
+        favoritesHTML += favoriteCountryHTML;
+    });
+
+    favoritesHTML += '</div>';
+    tabFavorites.innerHTML = favoritesHTML;
+}
+
+function renderSummary() {
+    countCountries.textContent = allCountries.length;
+    countFavorites.textContent = favoriteCountries.length;
+
+    const totalPopulation = allCountries.reduce((accumulator, current) => {
+        return accumulator + current.population;
+    }, 0);
+
+    const totalFavorites = favoriteCountries.reduce((accumulator, current) => {
+        return accumulator + current.population;
+    }, 0);
+
+    totalPopulationList.textContent = formatNumber(totalPopulation);
+    totalPopulationFavorites.textContent = formatNumber(totalFavorites);
+}
+
+function handleCountryButtons() {
+    const countryButtons = Array.from(tabCountries.querySelectorAll('.btn'));
+    const favoriteButtons = Array.from(tabFavorites.querySelectorAll('.btn'));
+
+    countryButtons.forEach(button => {
+        button.addEventListener('click', () => addToFavorites(button.id));
+    });
+
+    favoriteButtons.forEach(button => {
+        button.addEventListener('click', () => removeFromFavorites(button.id));
     });
 }
-function renderFavorites() { }
-function renderSumary() { }
-function handleCountryButtons() { }
+
+function addToFavorites(id) {
+    const countryToAdd = allCountries.find(country => country.id === id);
+
+    favoriteCountries = [...favoriteCountries, countryToAdd];
+
+    favoriteCountries.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    allCountries = allCountries.filter(country => country.id !== id);
+
+    render();
+}
+
+function removeFromFavorites(id) {
+    const countryToRemove = favoriteCountries.find(country => country.id === id);
+
+    allCountries = [...allCountries, countryToRemove];
+
+    allCountries.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    favoriteCountries = favoriteCountries.filter(country => country.id !== id);
+
+    render();
+}
+
+function formatNumber(number) {
+    return numberFormat.format(number);
+}
